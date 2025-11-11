146aa0c8a47cd48cdf6feba4f90076e11d29064a
ferreirandre <adrecruz15@gmail.com>
2025-11-11 00:06:48 -0300

    adding api skeleton

---
+++ coffee-brewing-methods-api/src/main/java/ferreirandre/github/coffee_brewing_methods/CoffeeBrewingMethodsApplication.java
@@ -0,0 +1,13 @@
+package ferreirandre.github.coffee_brewing_methods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeBrewingMethodsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeBrewingMethodsApplication.class, args);
+	}
+
+}
---
+++ coffee-brewing-methods-api/src/test/java/ferreirandre/github/coffee_brewing_methods/CoffeeBrewingMethodsApplicationTests.java
@@ -0,0 +1,13 @@
+package ferreirandre.github.coffee_brewing_methods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeBrewingMethodsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
